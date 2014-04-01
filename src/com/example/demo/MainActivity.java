5f7fa8d0f72044e81b2d6c8eb581016a5987598e
Read <read@fu101.com>
2014-04-01 18:00:22 +0800

    ghh

--- src/com/example/demo/MainActivity.java
+++ src/com/example/demo/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//dingjunta123123o
+//dingjunta123123oxcxdvxdv
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
